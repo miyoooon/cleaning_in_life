8ec8acc3c2c96540ac84de857ceecd3f66025771
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-09-11 13:35:26 +0000

    [Add] user/session admin/session

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  resources :posts
+  scope module: :public do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
