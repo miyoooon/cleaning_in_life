fb66b1145343915942bd90dc4c6af7f669baf6ee
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-09-16 11:50:21 +0000

    [Add] users/edit users/update users/withdraw

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,12 +5,29 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    @user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(@user.id)
+    end
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
-  def destroy
+  def withdraw
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to  new_user_registration_path
+  end
+
+  private
+  def user_params
+  params.require(:user).permit(:name, :introduce)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   scope module: :public do
     resources :posts
-    resources :users, only: [:show, :edit, :update, :destroy]
+    resources :users, only: [:show, :edit, :update]
+    patch "/users/:id/withdraw" => "users#withdraw", as: "user_withdraw"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
