b4120b7c3381005174fc67e004391800182aeee6
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-09-18 09:55:43 +0000

    [Add] public/posts/edit public/posts/destroy

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -22,12 +22,22 @@ def show
 end
 
 def edit
+  @post = Post.find(params[:id])
+   unless @post.user_id == current_user.id
+      redirect_to post_path(@post.id)
+    end
 end
 
 def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post.id)
 end
 
 def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
 end
 
 private
