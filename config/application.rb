c19e39529482c7054472b0022df4072bc509eda8
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-10-25 12:27:35 +0000

    [Fix] scss

--- config/application.rb
+++ config/application.rb
@@ -20,3 +20,12 @@ module CleaningInLife
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+module CleaningInLife
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja #追加
+  end
+end
